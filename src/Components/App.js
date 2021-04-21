5acf1cc2eea2766b9af5e4a9d0582ff4ba97c2f2
unknown <kharelsabin467@gmail.com>
2021-04-21 17:21:34 +0545

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import {Button} from "antd";
+
+import "antd/dist/antd.less";
+import "../styles/index.scss";
+import './App.scss'
+const App = () => {
+    return (
+        <section>
+            <p>App Running Good.</p>
+            <Button type={'primary'}>Antd Button</Button>
+        </section>
+    );
+};
+
+export default App;
+ 
